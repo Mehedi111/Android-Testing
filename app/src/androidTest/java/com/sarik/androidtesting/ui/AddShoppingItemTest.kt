6219aca9a5f4960933b851c9edff15cf53a2fdec
Sarik111 <gakk.hasan@gmail.com>
2021-01-04 10:44:42 +0600

    Insert item to db test

--- app/src/androidTest/java/com/sarik/androidtesting/ui/AddShoppingItemTest.kt
+++ app/src/androidTest/java/com/sarik/androidtesting/ui/AddShoppingItemTest.kt
@@ -1,24 +1,31 @@
 package com.sarik.androidtesting.ui
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.filters.MediumTest
+import com.google.common.truth.Truth.assertThat
 import com.sarik.androidtesting.R
+import com.sarik.androidtesting.data.local.ShoppingItem
+import com.sarik.androidtesting.getOrAwaitValue
 import com.sarik.androidtesting.lunchFragmentInHiltContainer
-import com.sarik.androidtesting.repositories.ShoppingRepository
+import com.sarik.androidtesting.repositories.FakseShoppingRepoAndroidTRest
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 import org.mockito.Mockito
 import org.mockito.Mockito.mock
+import javax.inject.Inject
 
 /**
  * Created by Mehedi Hasan on 12/30/2020.
@@ -32,8 +39,14 @@ class AddShoppingItemTest {
     @get:Rule
     val hiltRule = HiltAndroidRule(this)
 
+    @get: Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Inject
+    lateinit var fragmentFactory: ShoppingFragmentFactory
+
     @Before
-    fun setUp(){
+    fun setUp() {
         hiltRule.inject()
     }
 
@@ -49,7 +62,28 @@ class AddShoppingItemTest {
     }
 
     @Test
-    fun clickShoppingImage_navigateImagePickFragment(){
+    fun clickInsertIntoDb_shoppingItemInsertedIntoDB() {
+        val testViewModel = ShoppingViewModel(FakseShoppingRepoAndroidTRest())
+        lunchFragmentInHiltContainer<AddShoppingItem>(
+            fragmentFactory = fragmentFactory
+        ) {
+            this.viewModel = testViewModel
+        }
+
+        onView(withId(R.id.etShoppingItemName)).perform(replaceText("shopping item"))
+        onView(withId(R.id.etShoppingItemAmount)).perform(replaceText("5"))
+        onView(withId(R.id.etShoppingItemPrice)).perform(replaceText("5.5"))
+        onView(withId(R.id.btnAddShoppingItem)).perform(click())
+
+
+        assertThat(
+            testViewModel.shoppingItems.getOrAwaitValue()
+                .contains(ShoppingItem("shopping item", 5, 5.5f, ""))
+        )
+    }
+
+    @Test
+    fun clickShoppingImage_navigateImagePickFragment() {
         val navController = mock(NavController::class.java)
         lunchFragmentInHiltContainer<AddShoppingItem> {
             Navigation.setViewNavController(requireView(), navController)
@@ -61,16 +95,5 @@ class AddShoppingItemTest {
         )
     }
 
-    /*@Test
-    fun pressBack_makeImageUrlEmpty(){
-        val navController = mock(NavController::class.java)
-        lunchFragmentInHiltContainer<AddShoppingItem> {
-            Navigation.setViewNavController(requireView(), navController)
-        }
-        Espresso.pressBack()
-        Mockito.verify(navController).popBackStack()
 
-        val viewModel = Mockito.mock(ShoppingViewModel::class.java)
-        Mockito.verify(viewModel).setCurrentImageUrl("")
-    }*/
 }
--- app/src/main/java/com/sarik/androidtesting/ui/AddShoppingItem.kt
+++ app/src/main/java/com/sarik/androidtesting/ui/AddShoppingItem.kt
@@ -6,24 +6,41 @@ import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.androiddevs.shoppinglisttestingyt.other.Status
+import com.bumptech.glide.RequestManager
+import com.google.android.material.snackbar.Snackbar
 import com.sarik.androidtesting.R
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_add_shopping_item.*
+import javax.inject.Inject
 
 /**
  * Created by Mehedi Hasan on 11/24/2020.
  */
-class AddShoppingItem: Fragment(R.layout.fragment_add_shopping_item) {
-    private lateinit var viewModel: ShoppingViewModel
+class AddShoppingItem @Inject constructor(
+    val glide: RequestManager
+) : Fragment(R.layout.fragment_add_shopping_item) {
+    lateinit var viewModel: ShoppingViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
 
+        subscribeToObservers()
+
+        btnAddShoppingItem.setOnClickListener {
+            viewModel.insertShoppingItem(
+                etShoppingItemName.text.toString(),
+                etShoppingItemPrice.text.toString(),
+                etShoppingItemAmount.text.toString()
+            )
+        }
+
         ivShoppingImage.setOnClickListener {
             findNavController().navigate(AddShoppingItemDirections.actionAddShoppingItemToImagePickFragment())
         }
-        val callback = object : OnBackPressedCallback(true){
+        val callback = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 viewModel.setCurrentImageUrl("")
                 findNavController().popBackStack()
@@ -32,5 +49,40 @@ class AddShoppingItem: Fragment(R.layout.fragment_add_shopping_item) {
         }
 
         requireActivity().onBackPressedDispatcher.addCallback(callback)
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.curImageUrl.observe(viewLifecycleOwner) {
+            try {
+                glide.load(it).into(ivShoppingImage)
+            } catch (e: Exception) {
+            }
+        }
+
+        viewModel.insertShoppingItemStatus.observe(viewLifecycleOwner, {
+            it.getContentIfNotHandled()?.let {
+                when (it.status) {
+                    Status.SUCCESS -> {
+                        Snackbar.make(
+                            requireActivity().rootLayout,
+                            "Sucessfully inserted",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                        findNavController().popBackStack()
+                    }
+                    Status.ERROR -> {
+                        Snackbar.make(
+                            requireActivity().rootLayout,
+                            it.message ?: "An unknown error occured !",
+                            Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                    Status.LOADING -> {
+                        /*NO OP*/
+                    }
+                }
+            }
+        })
+
     }
 }
